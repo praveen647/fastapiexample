9a1fc80043821c80ee793b66bf9f2a1a362c2d2e
praveen647 <133590552+praveen647@users.noreply.github.com>
2024-09-15 00:49:31 +0530

    Update main.py

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ from firebase_admin import credentials
 from firebase_admin import firestore
 
 
-cred = credentials.Certificate("secrets.json")
+cred = credentials.Certificate(os.getenv('SERVICES')
 firebase_admin.initialize_app(cred)
 db = firestore.client()
 collection_name = os.getenv('COLLECTION_NAME')
